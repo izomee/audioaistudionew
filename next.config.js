14c150f39402f5a1301e05edffe8203cd1a99ba8
s <s>
2025-03-03 22:04:49 +0100

    Disable ESLint during builds for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    // Disable ESLint during build
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig; 
